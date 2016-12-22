5acad56b39e345616bd6e24b66bd637a3500c8b5
MasterHans <suvorov.ag.75@gmail.com>
2016-12-22 08:38:44 +0300

    End markups section

--- mail.php
+++ mail.php
@@ -5,8 +5,7 @@ $sitename = "Название сайта";
 
 $name = trim($_GET["name"]);
 $phone = trim($_GET["phone"]);
-$text = trim($_GET["text"]);
 
 $pagetitle = "Новая заявка с сайта \"$sitename\"";
-$message = "Имя: $name \nТелефон: $phone \nТекст: $text";
+$message = "Имя: $name \nТелефон: $phone";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
